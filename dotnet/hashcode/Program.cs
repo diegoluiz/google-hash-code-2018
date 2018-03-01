80b5963f70ce8046d9d24cefc98e27649bd0ce26
Diego Luiz <diegorcl@gmail.com>
2018-03-01 00:25:58 +0000

    refactoring

--- dotnet/hashcode/Program.cs
+++ dotnet/hashcode/Program.cs
@@ -17,72 +17,81 @@ namespace hashcode
             {
                 Log.Write("Processing file {0}", filePath);
                 var outputFilePath = filePath.Replace(".in", ".out");
-                // var filePath = args.Length >= 1 ? args[0] : "../data/example.in";
-                var file = File.ReadAllLines(filePath);
-                var info = file.First().Split(" ");
-                var pizza = new Pizza(file.Skip(1).ToList());
-                var sliceMap = new SliceMap(pizza.RowsCount, pizza.ColsCount);
 
-                Context.minIngredients = int.Parse(info[2]);
-                Context.maxItems = int.Parse(info[3]);
-                List<SliceType> sliceTypes = GetSliceTypes();
+                var sliceTypes = GetSliceTypes();
 
-
+                var pizza = ParseFile(filePath);
                 Console.WriteLine($"pizza with {pizza.RowsCount * pizza.ColsCount} cells");
                 Console.WriteLine($"MushroomCount={pizza.MushroomCount}");
                 Console.WriteLine($"TomatoCount={pizza.TomatoCount}");
 
-                foreach (var i in pizza.Grid)
-                {
-                    Log.Debug(string.Join("", i));
-                }
+                pizza.Print();
 
-                for (var row = 0; row < pizza.RowsCount; row++)
-                {
-                    for (var col = 0; col < pizza.ColsCount; col++)
-                    {
-                        foreach (var sliceType in sliceTypes)
-                        {
-                            if (row + sliceType.RowsCount > pizza.RowsCount || col + sliceType.ColsCount > pizza.ColsCount)
-                                continue;
+                MapPizza(ref pizza, sliceTypes);
 
-                            var row1 = row;
-                            var row2 = row + sliceType.RowsCount;
-                            var col1 = col;
-                            var col2 = col + sliceType.ColsCount;
-                            var components = new List<char>();
-                            for (var c = row; c < row2; c++)
-                            {
-                                for (var d = col; d < col2; d++)
-                                {
-                                    components.Add(pizza.Grid[c][d]);
-                                }
-                            }
-
-                            var slice = new Slice
-                            {
-                                Row1 = row1,
-                                Row2 = row2 - 1,
-                                Col1 = col1,
-                                Col2 = col2 - 1,
-                                Components = components
-                            };
-
-                            if (!slice.IsValidSlice())
-                                continue;
-
-                            if (sliceMap.IsSliceOverlapping(slice))
-                                continue;
-
-                            var sliceIndex = pizza.Slices.Count;
-                            pizza.Slices.Add(slice);
-                            sliceMap.Write(slice, sliceIndex);
-                        }
-                    }
-                }
-                sliceMap.Print();
                 PrintOutput(pizza, outputFilePath);
             }
+        }
+
+        private static void MapPizza(ref Pizza pizza, List<SliceType> sliceTypes)
+        {
+            var sliceMap = new SliceMap(pizza.RowsCount, pizza.ColsCount);
+            for (var row = 0; row < pizza.RowsCount; row++)
+            {
+                for (var col = 0; col < pizza.ColsCount; col++)
+                {
+                    foreach (var sliceType in sliceTypes)
+                    {
+                        if (row + sliceType.RowsCount > pizza.RowsCount || col + sliceType.ColsCount > pizza.ColsCount)
+                            continue;
+
+                        var slice = BuildSlice(pizza, row, col, sliceType);
+
+                        if (!slice.IsValidSlice())
+                            continue;
+
+                        if (sliceMap.IsSliceOverlapping(slice))
+                            continue;
+
+                        var sliceIndex = pizza.Slices.Count;
+                        pizza.Slices.Add(slice);
+                        sliceMap.Write(slice, sliceIndex);
+                    }
+                }
+            }
+            sliceMap.Print();
+        }
+
+        private static Slice BuildSlice(Pizza pizza, int row, int col, SliceType sliceType)
+        {
+            var row1 = row;
+            var row2 = row + sliceType.RowsCount;
+            var col1 = col;
+            var col2 = col + sliceType.ColsCount;
+            var components = new List<char>();
+
+            for (var c = row1; c < row2; c++)
+            {
+                for (var d = col; d < col2; d++)
+                {
+                    components.Add(pizza.Grid[c][d]);
+                }
+            }
+
+            var slice = new Slice { Row1 = row1, Row2 = row2 - 1, Col1 = col1, Col2 = col2 - 1, Components = components };
+            return slice;
+        }
+
+        private static Pizza ParseFile(string filePath)
+        {
+            var file = File.ReadAllLines(filePath);
+            var info = file.First().Split(" ");
+
+            Context.minIngredients = int.Parse(info[2]);
+            Context.maxItems = int.Parse(info[3]);
+
+            var pizza = new Pizza(file.Skip(1).ToList());
+            return pizza;
         }
 
         private static List<SliceType> GetSliceTypes()
@@ -99,14 +108,14 @@ namespace hashcode
                 }
             }
 
-            sliceTypes = sliceTypes.OrderByDescending(x => x.ColsCount * x.RowsCount).ToList();
+            sliceTypes = sliceTypes.OrderBy(x => x.ColsCount * x.RowsCount).ToList();
             return sliceTypes;
         }
 
         private static void PrintOutput(Pizza pizza, string filepath)
         {
             var sb = "";
-            Log.Write("--------------- OUTPUT ---------------");
+            Log.Debug("--------------- OUTPUT ---------------");
 
             sb += pizza.Slices.Count.ToString() + Environment.NewLine;
 
@@ -115,8 +124,8 @@ namespace hashcode
                 sb += string.Format("{0} {1} {2} {3}", slice.Row1, slice.Col1, slice.Row2, slice.Col2) + Environment.NewLine;
             }
 
-            Log.Write(sb);
-            Log.Write("--------------- OUTPUT ---------------");
+            Log.Debug(sb);
+            Log.Debug("--------------- OUTPUT ---------------");
 
             File.WriteAllText(filepath, sb);
         }
