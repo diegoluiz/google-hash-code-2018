af08969462f8484c8322b1b9b67b48d677323542
Salvatore Previti <roorback@gmail.com>
2018-03-01 20:00:20 +0000

    minor optimization in preparation of the next

--- code/RidePool.cs
+++ code/RidePool.cs
@@ -8,29 +8,29 @@ using System.Threading;
 
 namespace lasagnas {
   public class RidePool {
-    private List<Ride> _rides;
-    public RidePool () {
-      _rides = new List<Ride> ();
-    }
 
-    public void AddRide (Ride ride) {
-      _rides.Add (ride);
-      _rides = _rides.OrderBy (x => x.EarlierStart).ToList ();
-    }
+    private Ride[] _rides;
 
-    public int CountRide {
-      get {
-        return _rides.Count;
+    public RidePool(List<InputRow> rows) {
+      var rides = new Ride[rows.Count];
+      for (var i = 0; i < rides.Length; ++i) {
+        var c = rows[i];
+        rides[i] = new Ride(i, c.StartRow, c.StartColumn, c.FinishRow, c.FinishColumn, c.EarlierStart, c.LatestFinish);
       }
+      Array.Sort(rides, (a, b) => a.EarlierStart - b.EarlierStart);
+      this._rides = rides;
     }
 
-    public Ride GetBestRideFor (Car car) {
-      var availableRides = _rides.Where (x => !x.Assigned);
+    public int CountRide =>
+      _rides.Length;
+
+    public Ride GetBestRideFor(Car car) {
+      var availableRides = _rides.Where(x => !x.Assigned);
 
       var minDistance = int.MaxValue;
       Ride closestRideToCar = null;
       foreach (var ride in availableRides) {
-        var distanceFromStartingPoint = car.CurrentPosition.GetDistance (ride.Start);
+        var distanceFromStartingPoint = car.CurrentPosition.GetDistance(ride.Start);
         if (minDistance > distanceFromStartingPoint) {
           minDistance = distanceFromStartingPoint;
           closestRideToCar = ride;
