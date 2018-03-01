a144b2eccba5d6565ed991c7c105a7c2210564a3
Salvatore Previti <roorback@gmail.com>
2018-03-01 17:19:38 +0000

    finalized

--- code/Runner.cs
+++ code/Runner.cs
@@ -18,7 +18,24 @@ namespace lasagnas {
     public void Run() {
       Console.WriteLine($"* Running {Input.Name}");
 
-      Input.WriteOutput("hello");
+      PrintOutput();
+    }
+
+    private void PrintOutput() {
+      var sb = new StringBuilder();
+
+      //sb.AppendLine(pizza.Slices.Count.ToString());
+      //foreach (var slice in pizza.Slices) {
+      //  sb.AppendLine(string.Format("{0} {1} {2} {3}", slice.Row1, slice.Col1, slice.Row2, slice.Col2));
+      //}
+
+      var text = sb.ToString();
+
+      //Log.Debug("--------------- OUTPUT ---------------");
+      //Log.Debug(text);
+      //Log.Debug("--------------- OUTPUT ---------------");
+
+      Input.WriteOutput(text);
     }
   }
 }
